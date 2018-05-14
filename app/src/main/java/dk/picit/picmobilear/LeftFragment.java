639c733836087613b7f1545621a88e7ba592d4b5
stk <stk@PICit.dk>
2018-05-14 08:51:43 +0200

    Merge remote-tracking branch 'remotes/origin/Story6Camera'
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser

--- app/src/main/java/dk/picit/picmobilear/LeftFragment.java
+++ app/src/main/java/dk/picit/picmobilear/LeftFragment.java
@@ -1,8 +1,13 @@
 package dk.picit.picmobilear;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.params.StreamConfigurationMap;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
@@ -42,6 +47,16 @@ public class LeftFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         cameraInfo = new android.hardware.Camera.CameraInfo();
+        CameraManager manager = (CameraManager) getActivity().getSystemService(Context.CAMERA_SERVICE);
+        String camId = null;
+        try {
+            camId = manager.getCameraIdList()[0];
+            CameraCharacteristics cc = manager.getCameraCharacteristics(camId);
+            int cameraOrientation = cc.get(CameraCharacteristics.SENSOR_ORIENTATION);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Override
