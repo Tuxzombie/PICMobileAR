4557ef90e3a827cdfa4e0410f789af6ad4295d58
Kasper <kasper.j.k@gmail.com>
2018-05-22 12:08:59 +0200

    Check for internet connection

--- app/src/main/java/dk/picit/picmobilear/MainActivity.java
+++ app/src/main/java/dk/picit/picmobilear/MainActivity.java
@@ -22,6 +22,8 @@ import android.hardware.camera2.params.StreamConfigurationMap;
 import android.media.Image;
 import android.media.ImageReader;
 import android.media.MediaScannerConnection;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Environment;
 import android.os.SystemClock;
@@ -110,14 +112,13 @@ public class MainActivity extends AppCompatActivity {
 
         cameraFrameProvider.setDisplayOrientation(cameraOrientation);
 
-        try{
+        try {
             augumentaManager = AugumentaManager.getInstance(this, cameraFrameProvider);
         } catch (IllegalStateException e) {
             // Something went wrong while authenticating license
             Toast.makeText(this, "License error: " + e.getMessage(), Toast.LENGTH_LONG).show();
         }
     }
-
 
 
     @Override
@@ -145,7 +146,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private void startAugumentaManager(){
+    private void startAugumentaManager() {
         cameraFrameProvider.start();
         // add listener for pose 229
         augumentaManager.registerListener(showPoseListener, Poses.P229);
@@ -168,7 +169,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void requestCameraPermission() {
         // Request CAMERA permission from user
-        ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.CAMERA }, PERMISSION_REQUEST_CAMERA);
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_CAMERA);
     }
 
     private HandTransitionListener backTransitionListener = new HandTransitionListener() {
@@ -187,139 +188,132 @@ public class MainActivity extends AppCompatActivity {
 
     public void takePicture() {
 
-                stopAugumentaManager();
+        stopAugumentaManager();
 
-                final CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
-                try {
-                    String camId = manager.getCameraIdList()[0];
-                    CameraCharacteristics cc = manager.getCameraCharacteristics(camId);
-                    StreamConfigurationMap streamConfigs = cc.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
-                    Size[] jpegSizes = streamConfigs.getOutputSizes(ImageFormat.JPEG);
+        final CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            String camId = manager.getCameraIdList()[0];
+            CameraCharacteristics cc = manager.getCameraCharacteristics(camId);
+            StreamConfigurationMap streamConfigs = cc.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+            Size[] jpegSizes = streamConfigs.getOutputSizes(ImageFormat.JPEG);
 
-                    //find max resulution width
-                    int currentMax = jpegSizes[0].getWidth();
-                    int maxPlacement = 0;
-                    for(int i = 0; i < jpegSizes.length; i++)
-                    {
-                        if(currentMax < jpegSizes[i].getWidth()){
-                            currentMax = jpegSizes[i].getWidth();
-                            maxPlacement = i;
-                        }
+            //find max resulution width
+            int currentMax = jpegSizes[0].getWidth();
+            int maxPlacement = 0;
+            for (int i = 0; i < jpegSizes.length; i++) {
+                if (currentMax < jpegSizes[i].getWidth()) {
+                    currentMax = jpegSizes[i].getWidth();
+                    maxPlacement = i;
+                }
+            }
+
+            ImageReader jpegImageReader = ImageReader.newInstance(jpegSizes[maxPlacement].getWidth(), jpegSizes[maxPlacement].getHeight(), ImageFormat.JPEG, 1);
+            jpegImageReader.setOnImageAvailableListener(new ImageReader.OnImageAvailableListener() {
+                @Override
+                public void onImageAvailable(ImageReader reader) {
+                    Image image = reader.acquireNextImage();
+                    ByteBuffer buffer = image.getPlanes()[0].getBuffer();
+                    byte[] bytes = new byte[buffer.remaining()];
+                    buffer.get(bytes);
+                    FileOutputStream outImage = null;
+
+                    encodedImage = Base64.encodeToString(bytes, Base64.DEFAULT);
+
+                    ConnectivityManager connectivityManager = (ConnectivityManager) MainActivity.this.getSystemService(Context.CONNECTIVITY_SERVICE);
+                    NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+                    if(activeNetworkInfo == null || !activeNetworkInfo.isConnectedOrConnecting()){
+                        Toast toast = Toast.makeText(MainActivity.this, "No internet connection", Toast.LENGTH_LONG);
+                        toast.show();
+                    } else {
+                        VisionService visionService = new VisionService(getApplicationContext());
+                        visionService.execute(encodedImage);
                     }
 
-                    ImageReader jpegImageReader = ImageReader.newInstance(jpegSizes[maxPlacement].getWidth(), jpegSizes[maxPlacement].getHeight(), ImageFormat.JPEG, 1);
-                    jpegImageReader.setOnImageAvailableListener(new ImageReader.OnImageAvailableListener() {
-                        @Override
-                        public void onImageAvailable(ImageReader reader) {
-                            Image image = reader.acquireNextImage();
-                            ByteBuffer buffer = image.getPlanes()[0].getBuffer();
-                            byte[] bytes = new byte[buffer.remaining()];
-                            buffer.get(bytes);
-                            FileOutputStream outImage = null;
-//                            try {
-//                                File file = new File(Environment.getExternalStorageDirectory() + "/DCIM/Camera","pic"+ System.currentTimeMillis()+".jpg");
-//                                File encodedFile = new File(Environment.getExternalStorageDirectory() + "/DCIM/Camera", "encodedPic"+ System.currentTimeMillis()+".txt");
-//                                outImage = new FileOutputStream(file);
-//                                outImage.write(bytes);
-//                                sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.fromFile(file)));
+                }
+            }, null);
 
-                                encodedImage = Base64.encodeToString(bytes, Base64.DEFAULT);
-//                                PrintWriter outEncodedImage = new PrintWriter(encodedFile);
-//                                outEncodedImage.write(encodedImage);
-//                                outEncodedImage.close();
-                                VisionService visionService = new VisionService(getApplicationContext());
-                                visionService.execute(encodedImage);
+            mPreviewSurfaceTexture = new SurfaceTexture(1);
+            previewSurface = new Surface(mPreviewSurfaceTexture);
+            jpegCaptureSurface = jpegImageReader.getSurface();
 
-
-//                            } catch (IOException e) {
-//                                e.printStackTrace();
-//                            }
-                        }
-                    }, null);
-
-                    mPreviewSurfaceTexture = new SurfaceTexture(1);
-                    previewSurface = new Surface(mPreviewSurfaceTexture);
-                    jpegCaptureSurface = jpegImageReader.getSurface();
-
-                    if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                        //TODO: insert code for requesting permission if none avaliable
-                        return;
-                    }
-                    //Opens the camera
-                    manager.openCamera(camId, new CameraDevice.StateCallback() {
-                        //when opened ready the surfaces
-                        @Override
-                        public void onOpened(@NonNull CameraDevice camera) {
-                            mCamera = camera;
-                            List<Surface> surfaces = Arrays.asList(previewSurface, jpegCaptureSurface);
-                            try {
-                                //Creates session witch keeps requesting a picture for the preview service
-                                mCamera.createCaptureSession(surfaces, new CameraCaptureSession.StateCallback() {
-                                    @Override
-                                    public void onConfigured(@NonNull CameraCaptureSession session) {
-                                        mSession = session;
-                                        CaptureRequest.Builder request = null;
-                                        try {
-                                            request = mCamera.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
-                                            request.set(CaptureRequest.CONTROL_MODE, CaptureRequest.CONTROL_MODE_AUTO);
-                                            request.addTarget(previewSurface);
-                                            mSession.setRepeatingRequest(request.build(), new CameraCaptureSession.CaptureCallback() {
-                                                @Override
-                                                public void onCaptureCompleted(CameraCaptureSession session, CaptureRequest request, TotalCaptureResult result) {
-                                                    if(result.getFrameNumber() > 30)
-                                                    {
-                                                        try {
-                                                            mSession.abortCaptures();
-                                                        } catch (CameraAccessException e) {
-                                                            e.printStackTrace();
-                                                        }
-                                                    }
+            if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                //TODO: insert code for requesting permission if none avaliable
+                return;
+            }
+            //Opens the camera
+            manager.openCamera(camId, new CameraDevice.StateCallback() {
+                //when opened ready the surfaces
+                @Override
+                public void onOpened(@NonNull CameraDevice camera) {
+                    mCamera = camera;
+                    List<Surface> surfaces = Arrays.asList(previewSurface, jpegCaptureSurface);
+                    try {
+                        //Creates session witch keeps requesting a picture for the preview service
+                        mCamera.createCaptureSession(surfaces, new CameraCaptureSession.StateCallback() {
+                            @Override
+                            public void onConfigured(@NonNull CameraCaptureSession session) {
+                                mSession = session;
+                                CaptureRequest.Builder request = null;
+                                try {
+                                    request = mCamera.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+                                    request.set(CaptureRequest.CONTROL_MODE, CaptureRequest.CONTROL_MODE_AUTO);
+                                    request.addTarget(previewSurface);
+                                    mSession.setRepeatingRequest(request.build(), new CameraCaptureSession.CaptureCallback() {
+                                        @Override
+                                        public void onCaptureCompleted(CameraCaptureSession session, CaptureRequest request, TotalCaptureResult result) {
+                                            if (result.getFrameNumber() > 30) {
+                                                try {
+                                                    mSession.abortCaptures();
+                                                } catch (CameraAccessException e) {
+                                                    e.printStackTrace();
                                                 }
-                                            }, null);
-                                            request = mCamera.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE);
+                                            }
+                                        }
+                                    }, null);
+                                    request = mCamera.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE);
 //                                            request.set(CaptureRequest.CONTROL_MODE, CaptureRequest.CONTROL_MODE_AUTO);
 //                                            request.set(CaptureRequest.CONTROL_AF_MODE, CaptureRequest.CONTROL_AF_MODE_AUTO);
 //                                            request.set(CaptureRequest.CONTROL_AE_MODE, CaptureRequest.CONTROL_AE_MODE_ON);
-                                            request.addTarget(jpegCaptureSurface);
-                                            mSession.capture(request.build(), new CameraCaptureSession.CaptureCallback() {
-                                                @Override
-                                                public void onCaptureCompleted(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, @NonNull TotalCaptureResult result) {
-                                                    super.onCaptureCompleted(session, request, result);
-                                                    mCamera.close();
-                                                    startAugumentaManager();
-                                                }
-                                            }, null);
-                                        } catch (CameraAccessException e) {
-                                            e.printStackTrace();
+                                    request.addTarget(jpegCaptureSurface);
+                                    mSession.capture(request.build(), new CameraCaptureSession.CaptureCallback() {
+                                        @Override
+                                        public void onCaptureCompleted(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, @NonNull TotalCaptureResult result) {
+                                            super.onCaptureCompleted(session, request, result);
                                             mCamera.close();
                                             startAugumentaManager();
                                         }
-                                    }
-
-                                    @Override
-                                    public void onConfigureFailed(@NonNull CameraCaptureSession session) {
-
-                                    }
-                                }, null);
-                            } catch (CameraAccessException e) {
-                                e.printStackTrace();
-                                mCamera.close();
-                                startAugumentaManager();
+                                    }, null);
+                                } catch (CameraAccessException e) {
+                                    e.printStackTrace();
+                                    mCamera.close();
+                                    startAugumentaManager();
+                                }
                             }
-                        }
 
-                        @Override
-                        public void onDisconnected(@NonNull CameraDevice camera) {
+                            @Override
+                            public void onConfigureFailed(@NonNull CameraCaptureSession session) {
 
-                        }
-
-                        @Override
-                        public void onError(@NonNull CameraDevice camera, int error) {
-
-                        }
-                    }, null);
-                } catch (CameraAccessException e) {
-                    e.printStackTrace();
+                            }
+                        }, null);
+                    } catch (CameraAccessException e) {
+                        e.printStackTrace();
+                        mCamera.close();
+                        startAugumentaManager();
+                    }
                 }
-            }
+
+                @Override
+                public void onDisconnected(@NonNull CameraDevice camera) {
+
+                }
+
+                @Override
+                public void onError(@NonNull CameraDevice camera, int error) {
+
+                }
+            }, null);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
 }
